3835304a7ea1efe797bfd8740762cdda7180ed35
Hayden <asianhaydenxd@gmail.com>
2023-02-05 16:57:33 -0500

    allow multiple files and directories

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,18 @@ mod compiler;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Name of target file
-    filename: String,
+    /// Names of target files
+    filenames: Vec<String>,
 }
 
 
 fn main() {
     let args = Args::parse();
     
-    match file_reader::read_file(args.filename) {
-        Ok(filetext) => compiler::compile(filetext),
-        Err(error) => panic!("{}", error),
+    for filename in args.filenames {
+        match file_reader::read_file(filename) {
+            Ok(filetext) => compiler::compile(filetext),
+            Err(error) => panic!("{}", error),
+        }
     }
 }
