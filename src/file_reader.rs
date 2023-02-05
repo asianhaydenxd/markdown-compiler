838435b0437d91e7e7535ee3adab0e1ee5cb5382
Hayden <asianhaydenxd@gmail.com>
2023-02-05 16:54:05 -0500

    compile directories

--- src/file_reader.rs
+++ src/file_reader.rs
@@ -1,18 +1,30 @@
+use std::fs;
 use std::fs::File;
 use std::io::Error;
 use std::io::prelude::*;
 
 pub fn read_file(filename: String) -> Result<String, Error> {
-    match File::open(filename) {
-        Ok(mut file) => get_file_contents(&mut file),
+    match File::open(filename.clone()) {
+        Ok(mut file) => get_file_contents(filename, &mut file),
         Err(error) => Err(error),
     }
 }
 
-fn get_file_contents(file: &mut File) -> Result<String, Error> {
+fn get_file_contents(filename: String, file: &mut File) -> Result<String, Error> {
     let mut contents = String::new();
     match file.read_to_string(&mut contents) {
         Ok(_) => Ok(contents),
-        Err(error) => Err(error),
+        Err(_) => {
+            match fs::read_dir(filename) {
+                Ok(dir) => {
+                    let mut full_string = String::new();
+                    for subpath in dir {
+                        full_string.push_str(&read_file(subpath?.path().display().to_string())?);
+                    }
+                    Ok(full_string)
+                },
+                Err(error) => Err(error),
+            }
+        },
     }
 }
