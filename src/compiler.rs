03e1dc965d3fcd6daaea72da8fae4a1784bc7f06
Hayden <asianhaydenxd@gmail.com>
2023-02-05 16:20:10 -0500

    implement compiler

---
+++ src/compiler.rs
@@ -0,0 +1,63 @@
+use std::{fs::File, io::Write};
+
+pub fn compile(text: String) {
+    let chars: Vec<char> = text.chars().collect();
+    let mut index = 0;
+    while index < chars.len() {
+        if chars[index..=index+2] == "```".chars().collect::<Vec<char>>() {
+            render_file(chars.clone(), &mut index);
+        }
+        index += 1;
+    }
+}
+
+fn render_file(chars: Vec<char>, index: &mut usize) {
+    let mut delimiter_len = 0;
+    while matches!(chars.get(*index), Some('`')) {
+        *index += 1;
+        delimiter_len += 1;
+    }
+
+    while !matches!(chars.get(*index), Some( ' ' | '\n' | '\t' )) {
+        *index += 1;
+    }
+
+    while matches!(chars.get(*index), Some( ' ' | '\n' | '\t' )) {
+        *index += 1;
+    }
+
+    let file_name = get_word(chars.clone(), index);
+
+    let mut code = String::new();
+
+    loop {
+        if chars[*index] == '`' {
+            if chars[*index..*index+delimiter_len].iter().all(|x| *x == '`') {
+                *index += delimiter_len;
+                break;
+            }
+        }
+        code.push(chars[*index]);
+        *index += 1;
+    }
+
+    let mut file = File::create(file_name).unwrap();
+
+    file.write_all(code.as_bytes()).unwrap();
+}
+
+fn get_word(chars: Vec<char>, index: &mut usize) -> String {
+    let mut word: String = String::new();
+    
+    while !matches!(chars.get(*index), Some( ' ' | '\n' | '\t' )) {
+        word.push(chars[*index]);
+        *index += 1;
+    }
+
+    while chars[*index] != '\n' {
+        *index += 1;
+    }
+    *index += 1;
+
+    word
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
+use clap::Parser;
+
 mod file_reader;
+mod compiler;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of target file
+    filename: String,
+}
+
 
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    
+    match file_reader::read_file(args.filename) {
+        Ok(filetext) => compiler::compile(filetext),
+        Err(error) => panic!("{}", error),
+    }
 }
